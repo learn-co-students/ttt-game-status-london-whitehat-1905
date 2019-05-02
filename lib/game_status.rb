75a1ccfb8e14c64a67a14352be0e7d6ad7cec610
Alex Gyurov <alexgyurov@gmail.com>
2019-05-02 10:09:52 +0100

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,85 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+]
+
+def won?(board)
+  if (board.all? { |value| value == ' ' }) == true  # if board is empty
+    return false
+  end
+
+
+  if (WIN_COMBINATIONS.each do |combination|
+    if (board[combination[0]] == 'X') && (board[combination[1]] == 'X') && (board[combination[2]] == 'X')
+      return combination
+    end
+
+    if (board[combination[0]] == 'O') && (board[combination[1]] == 'O') && (board[combination[2]] == 'O')
+      return combination
+    end
+  end) != false
+    return false # draw
+  end
+
+end
+
+def full?(board)
+  if (board.all? { |value| value != ' ' }) == true
+    true
+  end
+end
+
+
+def draw?(board)
+  if full?(board) == true
+    if won?(board) == false
+      return true
+    end
+  end
+
+  false
+end
+
+def over?(board)
+  if won?(board)
+    return true
+  end
+
+  if draw?(board)
+    return true
+  end
+
+  if won?(board) == false && draw?(board) == false
+    return false
+  end
+end
+
+def winner(board)
+  combo = won?(board)
+
+  if combo
+    combo.each do |x|
+      if board[x] == 'X'
+        return 'X'
+      end
+
+      if board[x] == 'O'
+        return 'O'
+      end
+    end
+  end
+
+
+end
+
+board = ["X", "O", " ", " ", " ", " ", " ", "O", "X"]
+puts winner(board)
--- spec/game_status_spec.rb
+++ spec/game_status_spec.rb
@@ -32,7 +32,7 @@ describe "./lib/game_status.rb" do
     it 'returns an array of matching indexes for a top row win' do
       board = ["X", "X", "X", "O", "O", " ", " ", " ", " "]
 
-      expect(won?(board)).to match_array([0,1,2])
+      expect(won?(board)).to match_array([0, 1, 2])
     end
 
     it 'returns an array of matching indexes for a middle row win' do
